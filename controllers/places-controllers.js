ef7e10815f2664a35defd1cce7a46d032a910d4d
jenniferlindberg <jinjin.lindberg@gmail.com>
2021-11-22 22:15:14 +0100

    refactor: split code of routes into controllers * now routes only contain the routing * now controllers contain all functions of the routings

---
+++ controllers/places-controllers.js
@@ -0,0 +1,43 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Emp St Building",
+    description: "Famous skyscraper",
+    location: {
+      lat: 40.74,
+      lng: -73.98,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not fint place for the provided pID", 404); // will trigger error handling middleware
+  }
+  res.json({ place: place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  if (!place) {
+    return next(
+      new HttpError("Could not fint place for the provided uID", 404)
+    );
+  }
+  res.json({ place: place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,46 +1,11 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Emp St Building",
-    description: "Famous skyscraper",
-    location: {
-      lat: 40.74,
-      lng: -73.98,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-
-  if (!place) {
-    throw new HttpError("Could not fint place for the provided pID", 404); // will trigger error handling middleware
-  }
-  res.json({ place: place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-  if (!place) {
-    return next(
-      new HttpError("Could not fint place for the provided uID", 404)
-    );
-  }
-  res.json({ place: place });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
